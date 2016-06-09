69d4516c836e98707d33df8fcc7c040d825778a4
Robin Bron <robin@finwo.nl>
2016-06-09 10:43:36 +0200

    Project init

---
+++ README.md
@@ -0,0 +1,26 @@
+# finwo / cache
+
+[![Build Status](https://travis-ci.org/finwo/php-cache.svg?branch=master)](https://travis-ci.org/finwo/php-cache)
+
+-----
+
+Simple cache foundation for PHP
+
+-----
+
+### Contributing
+
+After checking the [Github issues](https://github.com/finwo/php-cache/issues) and confirming that your request isn't already being worked on, feel free to spawn a new fork of the develop branch & send in a pull request.
+
+
+The develop branch is merged periodically into the master after confirming it's stable, to make sure the master always contains a production-ready version.
+
+-----
+
+### Changelog
+- 2016-06-08
+    - Created README
+    - Added unit tests
+    - Added travis-ci build status
+- 2016-04-13
+    - Project init
---
+++ tests/LintTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class LintTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSrc()
+    {
+        // Main entry
+        $fileList = glob('src/');
+
+        // Loop through known files
+        while ( $inode = array_shift($fileList) ) {
+
+            // Try to iterate deeper
+            if ( is_dir($inode) ) {
+                $fileList = array_merge($fileList, glob(realpath($inode).'/*'));
+                continue;
+            }
+
+            // If we're a PHP file
+            if (preg_match('/^.+\.(php|inc)$/i', $inode)) {
+                // Run a unit test
+                $this->lintFile($inode);
+            }
+
+        }
+    }
+
+    private function lintFile($filename = '')
+    {
+        // Show progress in the terminal
+        print('.');
+
+        // And actually run the test (with proper error message)
+        $this->assertContains('No syntax errors', exec(sprintf('php -l "%s"', $filename), $out), sprintf("%s contains syntax errors", $filename));
+    }
+}
